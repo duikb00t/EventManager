d64611948e5f9b2a45cd20c4a5d338bbdfa3b846
Duikb00t <ward@ldl.be>
2017-03-27 15:14:25 +0200

    Ticket demo

--- src/Controllers/ScanController.php
+++ src/Controllers/ScanController.php
@@ -1,10 +1,16 @@
 <?php
 
+
 namespace Duikb00t\EventManager\Controllers;
 
 class ScanController extends BaseController {
 
 	public function validateTicket($ticketID) {
 		return 'Validating ticket ' . $ticketID;
+	}
+
+	public function loadTicket($id) {
+		echo sha1($id);
+		return View('eventmanager::tests.ticket');
 	}
 }
--- src/routes/routes.php
+++ src/routes/routes.php
@@ -5,6 +5,8 @@
 Route::group(['namespace' => '\Duikb00t\EventManager\Controllers'], function () {
 	Route::get('register', 'FormController@index');
 	Route::post('register', 'FormController@store')->name('register');
+	
+	Route::get('ticket/{id}', 'ScanController@loadTicket');
 });
 
 /**
@@ -13,7 +15,6 @@ Route::group(['namespace' => '\Duikb00t\EventManager\Controllers'], function () 
 Route::group(['prefix' =>'manager' ,'namespace' => '\Duikb00t\EventManager\Controllers'], function () {
 	Route::get('login', 'ManagerController@index');
 });
-
 
 /**
  * Scan at Even QR and valiate
